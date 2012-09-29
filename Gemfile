8a252bed79f9a1c6fead7e1027ec976dec4cfce8
railscard <comedian.card@gmail.com>
2012-09-29 20:34:39 +0400

    Added middleman (template=html5)

--- Gemfile
+++ Gemfile
@@ -1,18 +1,6 @@
-source "http://rubygems.org"
+source :rubygems
 
-group :development do
-  gem 'rake'
-  gem 'rack'
-  gem 'jekyll'
-  gem 'rdiscount'
-  gem 'pygments.rb'
-  gem 'RedCloth'
-  gem 'haml', '>= 3.1'
-  gem 'compass', '>= 0.11'
-  gem 'rubypants'
-  gem 'rb-fsevent'
-  gem 'stringex'
-  gem 'liquid', '2.2.2'
-end
-
-gem 'sinatra', '1.2.6'
+gem 'middleman', '~>3.0.5'
+gem 'rack-codehighlighter', :git => 'git://github.com/wbzyl/rack-codehighlighter.git'
+gem 'pygments.rb'
+gem 'redcarpet'
--- config.rb
+++ config.rb
@@ -1,16 +1,97 @@
-# Require any additional compass plugins here.
-project_type = :stand_alone
+###
+# Middleware
+###
+require 'rack/codehighlighter'
+require 'pygments'
 
-# Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+use Rack::Codehighlighter, 
+  :pygments,
+  :element => 'pre>code',
+  :pattern => /\A:::([-_+\w]+)\s*\n/,
+  :markdown => true
 
-# Local development paths
-sass_dir = "sass"
-images_dir = "source/images"
-fonts_dir = "source/fonts"
+set :markdown, :fenced_code_blocks => true,
+               :autolink => true, 
+               :smartypants => true
 
-line_comments = false
-output_style = :compressed
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install susy --pre
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+page "/*", :layout => "layout"
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'css'
+
+set :js_dir, 'js'
+
+set :images_dir, 'img'
+
+set :markdown_engine, :redcarpet
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
