1bc05fefa4fc0093044de77255f101fd99ead391
Andrey Deryabin <deriabin@gmail.com>
2014-01-16 17:42:09 +0400

    change contact email to info@rubynoname.ru

--- lib/feed_helpers.rb
+++ lib/feed_helpers.rb
@@ -5,8 +5,8 @@ module FeedHelpers
     {
       :title => "Ruby NoName podcast",
       :description => "Серия русскоязычных подкастов о языке Ruby, фреймворке Rails и различных других техонологиях, с ними связанными, начиная от PostgreSQL и Redis, и заканчивая JavaScript. Каждый выпуск состоит из двух частей — короткий блок новостей в мире Ruby, и длинный блок, состоящий из обсуждений и интервью с различными участинками русскоязычного Ruby-сообщества",
-      :author => "Иван Самсонов и Иван Евтухович",
-      :owner => {:author => "Иван Самсонов и Иван Евтухович", :email => "evtuhovich@gmail.com"},
+      :author => "Андрей Дерябин и Кир Шатров",
+      :owner => {:author => "Андрей Дерябин и Кир Шатров", :email => "info@rubynoname.ru"},
       :categories => {"Technology" => ["Podcasting", "Tech News"]},
       :image => "#{site_url}/images/big_logo.jpg",
       :explicit => "no"
